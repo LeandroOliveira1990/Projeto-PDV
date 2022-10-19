e51a5b19c0d70bd601ffb8f1691ced421e8ef158
Leandro de Oliveira Silva <leandrooliveirask8@hotmail.com>
2022-10-19 20:33:24 -0300

    Criação do método de listar clientes

--- src/main/java/dao/ClientesDAO.java
+++ src/main/java/dao/ClientesDAO.java
@@ -4,9 +4,13 @@
  */
 package dao;
 
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JOptionPane;
 import jdbc.ConnectionFactory;
 import model.Clientes;
@@ -67,6 +71,46 @@ public class ClientesDAO {
     
     //Metodo para excluir Cliente
     public void excluirCliente(){
+        
+    }
+    
+    //Metodo Listar Todos Clientes
+    public List<Clientes> listarClientes(){
+        try {
+            List<Clientes> lista = new ArrayList<>();
+            
+            String sql = "select * from tb_clientes";
+            PreparedStatement stmt = con.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+            
+            while(rs.next()){
+                Clientes obj = new Clientes();
+                obj.setId(rs.getInt("id"));
+                obj.setNome(rs.getString("nome"));
+                obj.setRg(rs.getString("rg"));
+                obj.setCpf(rs.getString("cpf"));
+                obj.setEmail(rs.getString("email"));
+                obj.setTelefone(rs.getString("telefone"));
+                obj.setCelular(rs.getString("celular"));
+                obj.setCep(rs.getString("cep"));
+                obj.setEndereco(rs.getString("endereco"));
+                obj.setNumero(rs.getInt("numero"));
+                obj.setComplemento(rs.getString("complemento"));
+                obj.setBairro(rs.getString("bairro"));
+                obj.setCidade(rs.getString("cidade"));
+                obj.setUf(rs.getString("uf"));
+                
+                lista.add(obj);
+                
+            }
+            
+            return lista;
+            
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null,"Erro: " +  e);
+            return null;
+        }
+        
         
     }
     
